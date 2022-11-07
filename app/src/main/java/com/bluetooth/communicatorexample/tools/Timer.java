b9b36da49c2fed7d5f7929e07f1611b82450f420
Pooja Babusing <poojababusing.k@thoughtworks.com>
2022-11-07 15:44:02 +0530

    [Pooja | Bishnu | Adityan] Add. bluetooth chat application

---
+++ app/src/main/java/com/bluetooth/communicatorexample/tools/Timer.java
@@ -0,0 +1,71 @@
+
+
+package com.bluetooth.communicatorexample.tools;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import com.bluetooth.communicator.tools.CustomCountDownTimer;
+
+
+public class Timer {
+    private final long hourMillis=3600000;
+    private final long minuteMillis=60000;
+    private final long secondMillis=1000;
+    private CustomCountDownTimer timer;
+    private long duration;
+    private Handler mainHandler;
+
+    public Timer(long durationMillis, final DateCallback dateCallback){
+        mainHandler = new Handler(Looper.getMainLooper());
+        duration=durationMillis;
+        timer= new CustomCountDownTimer(durationMillis,1000) {
+            @Override
+            public void onTick(long millisUntilEnd) {
+                int[] date=convertIntoDate(millisUntilEnd);
+                dateCallback.onTick(date[0],date[1],date[2]);
+            }
+
+            @Override
+            public void onFinish() {
+                dateCallback.onEnd();
+            }
+        };
+    }
+
+    public void start(){
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                timer.start();
+            }
+        });
+    }
+
+    private int[] convertIntoDate(long millis){
+        int hours=0;
+        int minutes=0;
+        int seconds=0;
+        if(millis>hourMillis){
+            long rest=millis%hourMillis;
+            hours= (int) ((millis-rest)/hourMillis);
+            millis=rest;
+        }
+        if(millis>minuteMillis){
+            long rest=millis%minuteMillis;
+            minutes= (int) ((millis-rest)/minuteMillis);
+            millis=rest;
+        }
+        if(millis>secondMillis){
+            long rest=millis%secondMillis;
+            seconds= (int) ((millis-rest)/secondMillis);
+        }
+
+        return new int[]{hours,minutes,seconds};
+    }
+
+    public interface DateCallback {
+        void onTick(int hoursUntilEnd, int minutesUntilEnd, int secondsUntilEnd);
+        void onEnd();
+    }
+}
---
+++ app/src/main/java/com/bluetooth/communicatorexample/tools/Tools.java
@@ -0,0 +1,19 @@
+
+package com.bluetooth.communicatorexample.tools;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.core.content.ContextCompat;
+
+public class Tools {
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
